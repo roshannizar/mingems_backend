8963be819a7349542389550c47481b2bd26baf3b
Roshan Nizar <roshannizar8@gmail.com>
2021-12-13 15:58:02 +0530

    added migration

---
+++ Mingems.Infrastructure/Migrations/20211213102645_Added-Investment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mingems.Infrastructure.Migrations
+{
+    public partial class AddedInvestment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Investments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    ModificationDate = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    RecordState = table.Column<int>(nullable: false),
+                    RefId = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    TransactionDate = table.Column<DateTime>(nullable: false),
+                    ContactNo = table.Column<string>(nullable: true),
+                    Amount = table.Column<decimal>(nullable: false),
+                    RemainingAmount = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Investments");
+        }
+    }
+}
