5e0eb7f34068d9bc3762a8dee054bb531211e0b7
Roshan Nizar <roshannizar8@gmail.com>
2021-12-19 01:31:42 +0530

    user api bug fixed

---
+++ Mingems.Api/Dtos/Users/UpdateUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mingems.Api.Dtos.Users
+{
+    public class UpdateUserDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
