6905216e76442680f0d8397d6dbab8aa1dbee043
Roshan Nizar <roshannizar8@gmail.com>
2022-02-06 19:41:30 +0530

    added record state

--- Mingems.Api/Dtos/Inventories/ImageLinesDto.cs
+++ Mingems.Api/Dtos/Inventories/ImageLinesDto.cs
@@ -1,9 +1,12 @@
-﻿namespace Mingems.Api.Dtos.Inventories
+﻿using Mingems.Shared.Core.Enums;
+
+namespace Mingems.Api.Dtos.Inventories
 {
     public class ImageLinesDto
     {
         public string Id { get; set; }
         public string URL { get; set; }
         public string InventoryId { get; set; }
+        public RecordState RecordState { get; set; }
     }
 }
