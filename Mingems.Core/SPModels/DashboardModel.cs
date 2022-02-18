9775e8b75393f0672ec4f78711108950d2a96027
Roshan Nizar <roshannizar8@gmail.com>
2022-02-18 11:10:55 +0530

    added email address

--- Mingems.Core/SPModels/DashboardModel.cs
+++ Mingems.Core/SPModels/DashboardModel.cs
@@ -27,6 +27,7 @@ namespace Mingems.Core.SPModels
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
         public decimal TotalAmount { get; set; }
     }
 }
